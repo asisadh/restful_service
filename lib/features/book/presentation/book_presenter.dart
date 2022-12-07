f3d1b59d7154de4a3025b6c63ff9db8934f51a89
Aashish Adhikari <asis.adh@gmail.com>
2022-12-07 14:50:46 -0500

    Book Presenter Created.

---
+++ lib/features/book/presentation/book_presenter.dart
@@ -0,0 +1,57 @@
+import 'package:clean_framework/clean_framework_providers.dart';
+import 'package:flutter/material.dart';
+import 'package:restful_service/features/book/domain/book_model.dart';
+import 'package:restful_service/features/book/domain/book_usecase.dart';
+import 'package:restful_service/features/book/presentation/book_view_model.dart';
+import 'package:restful_service/providers.dart';
+
+class BookPresenter
+    extends Presenter<BookViewModel, BookUIOutput, BookUseCase> {
+  BookPresenter({super.key, required PresenterBuilder<BookViewModel> builder})
+      : super(
+          builder: builder,
+          provider: bookUseCaseProvider,
+        );
+
+  @override
+  void onLayoutReady(BuildContext context, BookUseCase useCase) {
+    useCase.fetchBooks();
+  }
+
+  @override
+  BookViewModel createViewModel(
+    BookUseCase useCase,
+    BookUIOutput output,
+  ) {
+    return BookViewModel(
+      isLoading: output.isLoading,
+      books: output.books
+          .map((e) => e.toSingleBookViewModel())
+          .toList(growable: false),
+      errorMessage: output.errorMessage,
+      fetchBooks: useCase.fetchBooks,
+    );
+  }
+
+  @override
+  void onOutputUpdate(BuildContext context, BookUIOutput output) {
+    if (output.errorMessage.isNotEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(output.errorMessage)),
+      );
+    }
+  }
+}
+
+extension BookInputMappingToSingleBookViewModel on BookInput {
+  SingleBookViewModel toSingleBookViewModel() {
+    return SingleBookViewModel(
+      title: title,
+      subTitle: subTitle,
+      isbn: isbn,
+      price: price,
+      image: image,
+      url: url,
+    );
+  }
+}
